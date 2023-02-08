4e42d3267e5a268a56722930245fda801c687e3d
Maximilian Osinski <maximilianosinski@maxeman.com>
2023-02-08 12:53:37 +0100

    Commit.

---
+++ CrossDrop/Connection.cs
@@ -0,0 +1,113 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CrossDrop;
+
+public class Connection
+{
+    private TcpClient TcpClient { get; }
+    private NetworkStream NetworkStream { get; }
+    private string IpAddress { get; set; }
+    private int? Port { get; set; }
+    
+    public Connection(TcpClient tcpClient, NetworkStream networkStream, string ipAddress, int port)
+    {
+        TcpClient = tcpClient;
+        NetworkStream = networkStream;
+        IpAddress = ipAddress;
+        Port = port;
+    }
+
+    public static async Task<Connection> Initialize(string ipAddress, int port, int timeout = 60000)
+    {
+        var task = Ini(ipAddress, port);
+        if (await Task.WhenAny(task, Task.Delay(timeout)) == task)
+        {
+            return task.Result;
+        }
+
+        throw new TimeoutException("Connection timeout.");
+    }
+
+    public static async Task<Connection> FindConnectionAsync()
+    {
+        var ip = GetLocalIp();
+        if (ip == null)
+        {
+            throw new IOException("Network error.");
+        }
+
+        var index = ip.LastIndexOf(".", StringComparison.Ordinal);
+        var router = ip[..(index + 1)];
+        var pos = 49;
+
+        while (true)
+        {
+            try
+            {
+                if (pos >= 254)
+                {
+                    pos = 1;
+                }
+                pos++;
+                return await Initialize($"{router}{pos}", 11000, 2000);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    }
+
+    private static string? GetLocalIp()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        return (from ip in host.AddressList where ip.AddressFamily == AddressFamily.InterNetwork select ip.ToString()).FirstOrDefault();
+    }
+
+    private static async Task<Connection> Ini(string ipAddress, int port)
+    {
+        var client = new TcpClient();
+        while (true)
+        {
+            try
+            {
+                await client.ConnectAsync(IPAddress.Parse(ipAddress), port);
+                break;
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        return new Connection(client, client.GetStream(), ipAddress, port);
+    }
+    
+    public async Task SendFile(string filename, byte[] data)
+    {
+        var message = $"FILE:{data.Length}:{filename}";
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        await NetworkStream.WriteAsync(messageBytes);
+        await NetworkStream.WriteAsync(data);
+    }
+
+    public IPAddress? GetIpAddress()
+    {
+        return IpAddress == null ? null : IPAddress.Parse(IpAddress);
+    }
+
+    public int? GetPort()
+    {
+        return Port;
+    }
+
+    public void Disconnect()
+    {
+        TcpClient.Close();
+        NetworkStream.Close();
+        IpAddress = null;
+        Port = null;
+    }
+}
--- CrossDrop/MainPage.xaml.cs
+++ CrossDrop/MainPage.xaml.cs
@@ -1,9 +1,132 @@
-﻿namespace CrossDrop;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CrossDrop;
 
 public partial class MainPage
 {
+    private Connection _connection;
+    private byte[] _currentFile;
+    private string _currentFileName;
+
     public MainPage()
     {
         InitializeComponent();
+        var webView = new WebView();
+        ContentPage.Content = webView;
+        webView.Reload();
+        webView.Source = "http://192.168.108.212:5173";
+        webView.Navigating += async (_, args) =>
+        {
+            if (args.Url.Contains("action."))
+            {
+                args.Cancel = true;
+                var command = args.Url.Replace("action.", "");
+                var parts = command.Split(":", 3);
+                var action = parts[0];
+                var type = parts[1];
+                var identifier = parts[2];
+
+                if (action == "event")
+                {
+                    if (type == "command")
+                    {
+                        if (identifier == "searchDevice")
+                        {
+                            var listener = new Thread(Listener);
+                            listener.Start();
+                            _connection = await Connection.Initialize("127.0.0.1", 11000);
+                            var ipAddress = _connection.GetIpAddress()!.ToString();
+                            await webView.EvaluateJavaScriptAsync(
+                                $"actionHandler.resultEvent('command:searchDevice', '{JsonConvert.SerializeObject(new { ipAddress })}')");
+                        }
+                        
+                        if (identifier == "selectFile")
+                        {
+                            var result = await FilePicker.Default.PickAsync();
+                            if (result != null)
+                            {
+                                var stream = await result.OpenReadAsync();
+                                var memoryStream = new MemoryStream();
+                                await stream.CopyToAsync(memoryStream);
+                                _currentFile = memoryStream.ToArray();
+                                _currentFileName = result.FileName;
+                                await webView.EvaluateJavaScriptAsync(
+                                    $"actionHandler.resultEvent('command:selectFile', '{JsonConvert.SerializeObject(new { name = result.FileName })}')");
+                            }
+                        }
+
+                        if (identifier == "sendFile")
+                        {
+                            if (_currentFile == null || _currentFileName == null) return;
+                            await _connection.SendFile(_currentFileName, _currentFile);
+                            _currentFile = null;
+                            _currentFileName = null;
+                            await webView.EvaluateJavaScriptAsync($"actionHandler.resultEvent('command:sendFile', '{JsonConvert.SerializeObject(new { success = true })}')");
+                        }
+                    }
+                }
+            }
+        };
+    }
+
+    private async void Listener()
+    {
+        var server = new TcpListener(IPAddress.Any, 11000);
+        server.Start();
+
+        while (true)
+        {
+            var client = await server.AcceptTcpClientAsync();
+            var networkStream = client.GetStream();
+            var memoryStream = new MemoryStream();
+
+            var buffer = new byte[1024 * 16];
+            int bytesReceived;
+
+            long completeSize = 0;
+            var fileName = string.Empty;
+
+            while ((bytesReceived = networkStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                if (IsString(buffer))
+                {
+                    var message = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+                    if (!message.StartsWith("FILE")) continue;
+                    var messageArray = message.Split(':');
+                    completeSize = Convert.ToInt64(messageArray[1]);
+                    fileName = messageArray[2];
+                }
+                else
+                {
+                    await memoryStream.WriteAsync(buffer.AsMemory(0, bytesReceived));
+                    if (completeSize != memoryStream.ToArray().Length) continue;
+                    var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), fileName);
+                    await File.WriteAllBytesAsync(
+                        path,
+                        memoryStream.ToArray());
+                    completeSize = 0;
+                    fileName = string.Empty;
+                    memoryStream.Close();
+                    memoryStream = new MemoryStream();
+                    Array.Clear(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+
+    private static bool IsString(byte[] source)
+    {
+        try
+        {
+            new UTF8Encoding(false, true).GetCharCount(source);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
