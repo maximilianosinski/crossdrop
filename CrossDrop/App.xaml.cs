0a5ddbf7777acff17e7d3ac44e0d7c32f3b25c56
Maximilian Osinski <maximilianosinski@maxeman.com>
2023-02-08 12:00:02 +0100

    First commit.

---
+++ CrossDrop/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CrossDrop;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ CrossDrop/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CrossDrop;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
